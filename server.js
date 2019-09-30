c28c1235fd8504ed09c9a5270f244f31729d0e01
Owen Ripley <ojripley@gmail.com>
2019-09-30 12:14:37 -0400

    completed functionality for _header.ejs, urls_show.ejs and urls_index.ejs

--- server.js
+++ server.js
@@ -29,8 +29,12 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+
+// the colon in this address makes the following a VARIABLE. so 'shortURL' is not translated literally
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+
+  // req.params.shortURL refers to the variable in the address. :efjfjefojef becomes a paramater when the address is parsed
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
 
