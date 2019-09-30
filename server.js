8aaee696bfe66aaedb4d8b4d211c10ecd311e1f3
Owen Ripley <ojripley@gmail.com>
2019-09-30 08:40:29 -0400

    changed all request and response names to req and res for ease of typing

--- server.js
+++ server.js
@@ -11,61 +11,65 @@ app.listen(PORT, () => {
   console.log(`app is listening on port: ${PORT}`);
 });
 
-app.get('/', (request, response) => {
+app.get('/', (req, res) => {
 
   // if user is logged in, redirects to /urls
-  response.redirect('/urls');
+  res.redirect('/urls');
 
   // if user is not logged in, redirects to /login
-  response.redirect('/login');
+  res.redirect('/login');
 });
 
-app.get('/urls', (request, response) => {
-  response.send('welcome to urls!\n');
+app.get('/urls', (req, res) => {
+  res.send('welcome to urls!\n');
 });
 
-app.get('/login', (request, response) => {
-
-});
-
-app.get('/register', (request, response) => {
+app.get('urls.json', (req, res) => {
 
 });
 
-app.get('/urls/new', (request, response) => {
+app.get('/login', (req, res) => {
+
+});
+
+app.get('/register', (req, res) => {
+
+});
+
+app.get('/urls/new', (req, res) => {
 
   // if user is not logged in, redirects to /login
-  response.redirect('/login');
+  res.redirect('/login');
 });
 
-app.get('/urls/:id' , (request, response) => {
-
-});
-
-app.get('urls/:id/delete', (request, response) => {
+app.get('/urls/:id' , (req, res) => {
 
 });
 
-app.get('/u/:id', (request, response) => {
+app.get('urls/:id/delete', (req, res) => {
 
 });
 
-app.post('/urls' , (request, response) => {
+app.get('/u/:id', (req, res) => {
 
 });
 
-app.post('/urls/:id', (request, response) => {
+app.post('/urls' , (req, res) => {
 
 });
 
-app.post('/urls/:id/delete', (request, response) => {
+app.post('/urls/:id', (req, res) => {
 
 });
 
-app.post('/login', (request, response) => {
+app.post('/urls/:id/delete', (req, res) => {
 
 });
 
-app.post('/logout', (request, response) => {
+app.post('/login', (req, res) => {
+
+});
+
+app.post('/logout', (req, res) => {
 
 });
