49778b342e2083257589916641af08a820c5e8a3
Owen Ripley <ojripley@gmail.com>
2019-09-30 08:31:12 -0400

    npm init, outline of server.js file

--- server.js
+++ server.js
@@ -7,5 +7,61 @@ app.listen(port, () => {
 });
 
 app.get('/', (request, response) => {
-  response.send('welcome to shrink! enter a url to shorten');
+  
+
+  // if user is logged in, redirects to /urls
+  response.redirect('/urls');
+
+  // if user is not logged in, redirects to /login
+  response.redirect('/login');
+});
+
+app.get('/urls', (request, response) => {
+  response.send('welcome to urls!');
+});
+
+app.get('/login', (request, response) => {
+
+});
+
+app.get('/register', (request, response) => {
+
+});
+
+app.get('/urls/new', (request, response) => {
+
+  // if user is not logged in, redirects to /login
+  response.redirect('/login');
+});
+
+app.get('/urls/:id' , (request, response) => {
+
+});
+
+app.get('urls/:id/delete', (request, response) => {
+
+});
+
+app.get('/u/:id', (request, response) => {
+
+});
+
+app.post('/urls' , (request, response) => {
+
+});
+
+app.post('/urls/:id', (request, response) => {
+
+});
+
+app.post('/urls/:id/delete', (request, response) => {
+
+});
+
+app.post('/login', (request, response) => {
+
+});
+
+app.post('/logout', (request, response) => {
+
 });
